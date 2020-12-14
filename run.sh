4993f2d2269ddd87ceb7fb10b7fc49ec9cb0e2a5
Vincent MALLET <vmallet@thalassa.cm.cluster>
2020-12-14 12:15:50 +0100

    added the slurm script

---
+++ run.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+#SBATCH --job-name=train_tuiles
+#SBATCH --output=slurm_out/tt.out
+#SBATCH --error=slurm_out/tt.err
+#SBATCH -p gpu-cbio
+#SBATCH --gres=gpu:1 
+
+module load cuda10.1
+
+source ~/anaconda3/etc/profile.d/conda.sh
+conda activate histo
+
+
+python train.py --datadir data/test_0 --pretrained --model_name resnet50 --epochs 100 --batch_size 64
+
+
