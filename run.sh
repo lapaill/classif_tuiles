d453ee59d42b2514a7870ad660ac1a6e6e00bbe0
Vincent MALLET <vmallet@thalassa.cm.cluster>
2020-12-17 16:34:26 +0100

    small modif to include cpu resource in sbatch

--- run.sh
+++ run.sh
@@ -5,13 +5,13 @@
 #SBATCH -p gpu-cbio
 #SBATCH --gres=gpu:1 
 #SBATCH --mem=40000
+#SBATCH -c 10
 
 module load cuda10.1
 
 source ~/anaconda3/etc/profile.d/conda.sh
 conda activate histo
 
-
-python train.py --datadir data/test_0 --pretrained --model_name resnet50 --epochs 100 --batch_size 64
+python train.py --datadir data/test_0 --epochs 50 --batch_size 256 --model_name resnet18 --name first_resnet
 
 
