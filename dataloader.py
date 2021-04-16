8f324a987a504978fc91369b73220f639683d18e
Joseph Paillard <joseph.paillard@mines-paristech.fr>
2021-04-16 16:37:53 +0200

    add HDF5 dataloaders

--- dataloader.py
+++ dataloader.py
@@ -1,7 +1,39 @@
+import os
+
+import h5py
+import numpy as np
+import torch
+from PIL import Image
+from torch.utils.data import DataLoader, Dataset
 from torchvision.datasets import ImageFolder
 from torchvision.transforms import transforms
-from torch.utils.data import DataLoader
-import numpy as np
+
+
+class HDF5Dataset(Dataset):
+    """PyTorch Dataset from HDF5 files"""
+
+    def __init__(self, data_file, labels_file, transform=None):
+        """
+        Args:
+            data_file (string): Path to the HDF5 file with images. The dataset key must be 'x'
+            labels_file (string): Path to the HDF5 file with labels. The labels key must be 'y'
+            transform (callable, optional): Optional transform to be applied
+                on a sample.
+        """
+        self.data = h5py.File(data_file)['x']
+        self.labels = h5py.File(labels_file)['y']
+        self.transform = transform
+        self.classes = [0, 1]  # à modifier
+
+    def __len__(self):
+        return self.data.shape[0]
+
+    def __getitem__(self, idx):
+        x = Image.fromarray(self.data[idx])
+        y = int(self.labels[idx])
+        if self.transform:
+            x = self.transform(x)
+        return (x, y)
 
 
 class RandomRotate90(object):
@@ -19,8 +51,14 @@ class RandomRotate90(object):
 
 
 def get_dataloader(datadir, batch_size, pretrained, augmented):
-    dataset = ImageFolder(datadir, transform=get_transforms(pretrained, augmented))
-    dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True, num_workers=10)
+    #    dataset = ImageFolder(
+    #        datadir, transform=get_transforms(pretrained, augmented))
+    data = os.path.join(datadir, 'data.h5')
+    labels = os.path.join(datadir, 'labels.h5')
+    dataset = HDF5Dataset(
+        data, labels, transform=get_transforms(pretrained, augmented))
+    dataloader = DataLoader(dataset, batch_size=batch_size,
+                            shuffle=True, num_workers=10)
     return dataloader
 
 
@@ -34,7 +72,7 @@ def get_transforms(pretrained, augmented):
     if augmented:
         print('Use Augmentation plus')
         trans = transforms.Compose([
-            transforms.RandomResizedCrop(256),
+            #            transforms.RandomResizedCrop(256),
             transforms.RandomHorizontalFlip(),
             transforms.RandomVerticalFlip(),
             RandomRotate90(),
